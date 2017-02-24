7b934ebc70af03f18f437ac3cab31a4424eb71e9
Maksymilian <darkey999@yahoo.com>
2017-02-24 19:06:32 +0100

    Add EmptyView EmptyView shows text message if there's no earthquakes

--- app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
@@ -5,6 +5,8 @@ import android.content.Loader;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ListView;
+import android.widget.TextView;
+
 import java.util.ArrayList;
 
 public class EarthquakeActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<ArrayList<Earthquake>> {
@@ -14,13 +16,15 @@ public class EarthquakeActivity extends AppCompatActivity implements LoaderManag
     CustomEarthquakeAdapter adapter;
     ArrayList<Earthquake> earthquakes;
     ListView earthquakeListView;
-
+    TextView listTv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.earthquake_activity);
 
         earthquakeListView = (ListView) findViewById(R.id.list);
+        listTv = (TextView) findViewById(R.id.listTv);
+        earthquakeListView.setEmptyView(listTv);
 
         getLoaderManager().initLoader(0, null, this);
     }
@@ -36,6 +40,7 @@ public class EarthquakeActivity extends AppCompatActivity implements LoaderManag
     public void onLoadFinished(Loader<ArrayList<Earthquake>> loader, ArrayList<Earthquake> earthquakes) {
         adapter = new CustomEarthquakeAdapter(EarthquakeActivity.this, earthquakes);
         earthquakeListView.setAdapter(adapter);
+        listTv.setText(R.string.no_earthquakes_found);
     }
 
     // Function used when the loader is being reset
